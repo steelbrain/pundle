89b105a48b23bba08777a430971c2b99b0a369b3
steelbrain <de.steelbrain@gmail.com>
2016-04-03 17:22:07 -0700

    :new: Add mergeSourceMaps helper

--- packages/pundle/src/helpers.js
+++ packages/pundle/src/helpers.js
@@ -3,6 +3,7 @@
 /* @flow */
 
 import Path from 'path'
+import sourceMap from 'source-map'
 import type Pundle$Path from './path'
 import type { Pundle$Config, Pundle$FileSystem } from './types'
 
@@ -83,4 +84,43 @@ export async function getPlugins(
     }
   }
   return processed
+}
+
+// Source: https://goo.gl/821D9T
+export function mergeSourceMaps(inputMap: Object, map: Object): Object {
+  const inputMapConsumer   = new sourceMap.SourceMapConsumer(inputMap)
+  const outputMapConsumer  = new sourceMap.SourceMapConsumer(map)
+
+  const mergedGenerator = new sourceMap.SourceMapGenerator({
+    file: inputMapConsumer.file,
+    sourceRoot: inputMapConsumer.sourceRoot
+  })
+
+  // This assumes the output map always has a single source, since Babel always compiles a single source file to a
+  // single output file.
+  const source = outputMapConsumer.sources[0]
+
+  inputMapConsumer.eachMapping(function(mapping) {
+    const generatedPosition = outputMapConsumer.generatedPositionFor({
+      line: mapping.generatedLine,
+      column: mapping.generatedColumn,
+      source
+    })
+    if (typeof generatedPosition.column !== 'undefined') {
+      mergedGenerator.addMapping({
+        source: mapping.source,
+
+        original: {
+          line: mapping.originalLine,
+          column: mapping.originalColumn
+        },
+
+        generated: generatedPosition
+      })
+    }
+  })
+
+  const mergedMap = mergedGenerator.toJSON()
+  inputMap.mappings = mergedMap.mappings
+  return inputMap
 }
