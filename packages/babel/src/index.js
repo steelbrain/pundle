4a6ec8260a44e391dfebc4f557eb67822dd9157c
steelbrain <de.steelbrain@gmail.com>
2016-04-04 08:07:11 -0700

    :arrow_up: Upgrade babel transformer to latest API

--- packages/babel/src/index.js
+++ packages/babel/src/index.js
@@ -4,35 +4,32 @@
 
 import { transform } from 'babel-core'
 
-function getBabelTransformer(parameters: Object = {}): Function {
+function getBabelTransformer(pundle: Object, parameters: Object = {}) {
   if (!(parameters.ignored instanceof RegExp)) {
     parameters.ignored = /(node_modules|bower_components)/
   }
   if (!parameters.config || typeof parameters.config !== 'object') {
     parameters.config = {}
   }
+  pundle.observeCompilations(function(compilation) {
+    compilation.onBeforeCompile(function(event) {
+      if (event.filePath.match(parameters.ignored)) {
+        return
+      }
+      if (typeof parameters.include !== 'undefined' && !event.filePath.match(parameters.include)) {
+        return
+      }
+      const processed = transform(event.contents, Object.assign({}, parameters.config, {
+        filename: pundle.path.out(event.filePath),
+        sourceFileName: event.filePath,
+        inputSourceMap: event.sourceMap,
+        sourceMap: true
+      }))
 
-  return function(pundle: Object) {
-    pundle.observeCompilations(function(compilation) {
-      compilation.onBeforeCompile(function(event) {
-        if (event.filePath.match(parameters.ignored)) {
-          return
-        }
-        if (typeof parameters.include !== 'undefined' && !event.filePath.match(parameters.include)) {
-          return
-        }
-        const processed = transform(event.contents, Object.assign({}, parameters.config, {
-          filename: pundle.path.out(event.filePath),
-          sourceFileName: event.filePath,
-          inputSourceMap: event.sourceMap,
-          sourceMap: true
-        }))
-
-        event.contents = processed.code
-        event.sourceMap = processed.sourceMap
-      })
+      event.contents = processed.code
+      event.sourceMap = processed.sourceMap
     })
-  }
+  })
 }
 
 module.exports = getBabelTransformer
