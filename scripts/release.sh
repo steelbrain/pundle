6446c862cfcf1e4bef3b346a424ae7e0f4bc2bae
steelbrain <de.steelbrain@gmail.com>
2016-04-11 07:49:15 -0700

    :new: Add release script

---
+++ scripts/release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ACTION=$1
+
+# Usage: ./scripts/release.sh (major|minor|patch)
+
+ROOT_DIRECTORY=$( cd $(dirname $0) ; pwd -P )/..
+PACKAGES_PATH=${ROOT_DIRECTORY}/packages
+PACKAGES=( "babel" "dev" "fs" "middleware" "npm-installer" "pundle" )
+
+for name in "${PACKAGES[@]}"
+do :
+  cd ${PACKAGES_PATH}/${name}
+  npm version "${ACTION}" --no-git-tag-version
+  if [ $? -eq 1 ]; then
+    exit 1
+  fi
+  npm publish
+  if [ $? -eq 1 ]; then
+    exit 1
+  fi
+done
