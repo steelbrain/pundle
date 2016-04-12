cf5cfca02891f122a8842cc67d2009b199aae57f
steelbrain <de.steelbrain@gmail.com>
2016-04-11 18:42:59 -0700

    :new: Add version bumping script to release To make sure we no longer have the deployment issue we've hit twice

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,10 +13,11 @@ ${ROOT_DIRECTORY}/scripts/build.sh
 for name in "${PACKAGES[@]}"
 do :
   cd ${PACKAGES_PATH}/${name}
-  npm version "${ACTION}" --no-git-tag-version
+  VERSION=$(npm version "${ACTION}" --no-git-tag-version)
   if [ $? -eq 1 ]; then
     exit 1
   fi
+  node ${ROOT_DIRECTORY}/scripts/__bump_versions.js ${PACKAGES_PATH}/${name}/package.json ${VERSION}
   npm publish
   if [ $? -eq 1 ]; then
     exit 1
