9423238c800a42e64a4801e09d006053584a22dc
steelbrain <de.steelbrain@gmail.com>
2016-04-11 19:46:50 -0700

    :bug: Implement locks in npm installer Fixes #24

--- packages/npm-installer/src/index.js
+++ packages/npm-installer/src/index.js
@@ -19,25 +19,39 @@ function getNPMInstaller(pundle: Pundle, parameters: Object) {
   if (!(parameters.ignored instanceof RegExp)) {
     parameters.ignored = /(node_modules|bower_components)/
   }
+  const locks: Map<string, Promise> = new Map()
 
   pundle.path.onAfterModuleResolve(function(event) {
     if (event.path || (parameters.restrictToRoot && event.basedir.indexOf(pundle.config.rootDirectory) !== 0) || event.basedir.match(IGNORED)) {
       return null
     }
+    let lock = locks.get(event.moduleName)
+    if (lock) {
+      return lock.then(function(status) {
+        if (status) {
+          event.path = Path.join(parameters.rootDirectory, 'node_modules', event.moduleName)
+        }
+      })
+    }
     const id = ++nextID
     if (parameters.onBeforeInstall) {
       parameters.onBeforeInstall(id, event.moduleName)
     }
-    return installer.install(event.moduleName).then(function() {
+    lock = installer.install(event.moduleName).then(function() {
       event.path = Path.join(parameters.rootDirectory, 'node_modules', event.moduleName)
       if (parameters.onBeforeInstall) {
         parameters.onAfterInstall(id, event.moduleName, null)
       }
+      locks.delete(event.moduleName)
+      return true
     }, function(error) {
       if (parameters.onBeforeInstall) {
         parameters.onAfterInstall(id, event.moduleName, error)
       }
+      locks.delete(event.moduleName)
     })
+    locks.set(event.moduleName, lock)
+    return lock
   })
 }
 
