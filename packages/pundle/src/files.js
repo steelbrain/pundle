5041a5b39ae4fa7a8b742125de8f27c2b75abace
steelbrain <de.steelbrain@gmail.com>
2016-07-14 12:04:16 -0700

    :new: Add a Files class

---
+++ packages/pundle/src/files.js
@@ -0,0 +1,50 @@
+/* @flow */
+
+import invariant from 'assert'
+import { CompositeDisposable, Emitter } from 'sb-event-kit'
+import type { Disposable } from 'sb-event-kit'
+import PundlePath from './path'
+import { attachable } from './helpers'
+import type { Config, State } from './types'
+
+@PundlePath.attach
+@attachable('files')
+export default class Files {
+  path: PundlePath;
+  config: Config;
+  state: State;
+  files: Map<string, File>;
+  emitter: Emitter;
+  subscriptions: CompositeDisposable;
+
+  constructor(config: Config, state: State) {
+    this.config = config
+    this.state = state
+    this.files = new Map()
+    this.emitter = new Emitter()
+    this.subscriptions = new CompositeDisposable()
+
+    this.subscriptions.add(this.emitter)
+  }
+  has(path: string): boolean {
+    return this.files.has(this.path.in(path))
+  }
+  get(path: string): File {
+    const file = this.files.get(this.path.in(path))
+    invariant(file, 'File not found')
+    return file
+  }
+  set(path: string, file: File): void {
+    this.files.set(this.path.in(path), file)
+  }
+  onDidAdd(callback: ((filePath: string) => any)): Disposable {
+    return this.emitter.on('did-add', callback)
+  }
+  onDidRemove(callback: ((filePath: string) => any)): Disposable {
+    return this.emitter.on('did-remove', callback)
+  }
+  dispose() {
+    this.files.clear()
+    this.subscriptions.dispose()
+  }
+}
--- packages/pundle/src/index.js
+++ packages/pundle/src/index.js
@@ -1,14 +1,18 @@
 /* @flow */
 
+import Path from 'path'
+import invariant from 'assert'
 import { CompositeDisposable, Emitter } from 'sb-event-kit'
 import type { Disposable } from 'sb-event-kit'
 import * as Helpers from './helpers'
 import applyLoaders from './loaders'
+import Files from './files'
 import Resolver from './resolver'
 import PundlePath from './path'
 import FileSystem from './filesystem'
-import type { Config, State, File } from './types'
+import type { Config, State } from './types'
 
+@Files.attach
 @Resolver.attach
 @PundlePath.attach
 @FileSystem.attach
@@ -16,7 +20,7 @@ class Pundle {
   fs: FileSystem;
   path: PundlePath;
   state: State;
-  files: Map<string, File>;
+  files: Files;
   config: Config;
   emitter: Emitter;
   resolver: Resolver;
@@ -26,27 +30,36 @@ class Pundle {
     this.state = {
       loaders: new Map(),
     }
-    this.files = new Map()
     this.config = Helpers.fillConfig(config)
     this.emitter = new Emitter()
     this.subscriptions = new CompositeDisposable()
 
     this.subscriptions.add(this.emitter)
     applyLoaders(this)
+    // TODO: Use this.files's events to add or remove files from watcher, it'll be super-efficient that way as we'll only be watching what we required
   }
-  async read(givenFilePath: string, from: string): Promise<void> {
+  async read(givenFilePath: string): Promise<void> {
     const filePath = this.path.in(givenFilePath)
+    if (this.files.has(filePath) && !await this.fs.isChanged(givenFilePath)) {
+      return
+    }
+    const extension = Path.extname(filePath)
     const contents = await this.fs.read(filePath)
-    console.log(filePath, from, contents)
+    const loader = this.state.loaders.get(extension)
+    invariant(loader, `Unrecognized extension '${extension}' for '${givenFilePath}'`)
+    let result = loader(this.config, this.path.out(filePath), contents)
+    if (result && typeof result.then === 'function') {
+      result = await result
+    }
+    console.log(result)
   }
   async compile(): Promise<void> {
-    await Promise.all(this.config.entry.map(entry => this.read(entry, '$root')))
+    await Promise.all(this.config.entry.map(entry => this.read(entry)))
   }
   onError(callback: Function): Disposable {
     return this.emitter.on('error', callback)
   }
   dispose() {
-    this.files.clear()
     this.subscriptions.dispose()
   }
 }
