e278f7ac05f2edd9e9528754887675c83d1caca0
steelbrain <de.steelbrain@gmail.com>
2016-07-25 17:58:54 -0700

    :new: Add retry to HMR

--- packages/dev/browser.js
+++ packages/dev/browser.js
@@ -1,16 +1,25 @@
-const socket = new WebSocket(`${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}${PUNDLE.MAGIC.HMR_PATH}`)
-socket.addEventListener('open', function() {
-  console.log('[HMR] Connected')
-})
-socket.addEventListener('close', function() {
-  console.log('[HMR] Disconnected')
-})
-socket.addEventListener('message', function(event) {
-  const message = JSON.parse(event.data)
-  console.log('[HMR] message', message)
-  // if (message.type === 'update') {
-  //   console.log('[HMR] Applying', message.filePath)
-  //   eval(message.contents)
-  //   __sb_pundle_apply_hmr(message.filePath, new Set())
-  // }
-})
+'use strict'
+
+function openHMRConnection() {
+  const socket = new WebSocket(`${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}${PUNDLE.MAGIC.HMR_PATH}`)
+  socket.addEventListener('open', function() {
+    console.log('[HMR] Connected')
+  })
+  socket.addEventListener('close', function() {
+    console.log('[HMR] Disconnected')
+  })
+  socket.addEventListener('message', function(event) {
+    const message = JSON.parse(event.data)
+    console.log('[HMR] message', message)
+    // if (message.type === 'update') {
+    //   console.log('[HMR] Applying', message.filePath)
+    //   eval(message.contents)
+    //   __sb_pundle_apply_hmr(message.filePath, new Set())
+    // }
+  })
+  socket.addEventListener('close', function() {
+    console.log('[HMR] Retrying in 3 seconds')
+    setTimeout(openHMRConnection, 3000)
+  })
+}
+openHMRConnection()
