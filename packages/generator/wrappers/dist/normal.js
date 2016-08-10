a7e32e1abc65fe8d61009f0d4e1a490f7bedc70c
Steel Brain <i@steelbrain.me>
2016-08-09 19:50:09 -0700

    :art: Cleanup the normal wrapper

--- packages/generator/wrappers/dist/normal.js
+++ packages/generator/wrappers/dist/normal.js
@@ -1,28 +1,31 @@
 'use strict';
 
-var global = typeof window !== 'undefined' ? window : typeof self !== 'undefined' ? self : {},
-    GLOBAL = global,
-    root = global,
-    __SB_PUNDLE_DEFAULT_EXPORT = {},
-    __sb_pundle = {
+var global = typeof window !== 'undefined' && window || typeof self !== 'undefined' && self || {};
+
+var GLOBAL = global;
+var root = global;
+var __SB_PUNDLE_DEFAULT_EXPORT = {};
+var __sb_pundle = {
   cache: {},
   extensions: ['.js'],
   resolve: function resolve(path) {
     return path;
   }
 };
+var __require = void 0;
 
 function __sb_pundle_register(filePath, callback) {
   if (__sb_pundle.cache[filePath]) {
     __sb_pundle.cache[filePath].callback = callback;
   } else {
-    __sb_pundle.cache[filePath] = {
+    var module = {
       id: filePath,
       filePath: filePath,
       callback: callback,
       exports: __SB_PUNDLE_DEFAULT_EXPORT,
       parents: []
     };
+    __sb_pundle.cache[filePath] = module;
   }
 }
 
@@ -47,4 +50,4 @@ function __sb_generate_require(moduleName) {
   bound.resolve = __sb_pundle.resolve;
   return bound;
 }
-var __require = __sb_generate_require('$root');
+__require = __sb_generate_require('$root');
--- packages/generator/wrappers/normal.js
+++ packages/generator/wrappers/normal.js
@@ -1,25 +1,36 @@
-var
-  global = typeof window !== 'undefined' ? window : typeof self !== 'undefined' ? self : {},
-  GLOBAL = global,
-  root = global,
-  __SB_PUNDLE_DEFAULT_EXPORT = {},
-  __sb_pundle = {
-    cache: {},
-    extensions: ['.js'],
-    resolve: function(path) { return path },
-  }
+/* @flow */
+
+type Module = {
+  id: string,
+  filePath: string,
+  callback: Function,
+  exports: Object,
+  parents: Array<string>,
+}
+
+const global = (typeof window !== 'undefined' && window) || (typeof self !== 'undefined' && self) || {}
+const GLOBAL = global
+const root = global
+const __SB_PUNDLE_DEFAULT_EXPORT = {}
+const __sb_pundle = {
+  cache: {},
+  extensions: ['.js'],
+  resolve(path) { return path },
+}
+let __require
 
 function __sb_pundle_register(filePath, callback) {
   if (__sb_pundle.cache[filePath]) {
     __sb_pundle.cache[filePath].callback = callback
   } else {
-    __sb_pundle.cache[filePath] = {
+    const module: Module = {
       id: filePath,
-      filePath: filePath,
-      callback: callback,
+      filePath,
+      callback,
       exports: __SB_PUNDLE_DEFAULT_EXPORT,
       parents: [],
     }
+    __sb_pundle.cache[filePath] = module
   }
 }
 
@@ -27,7 +38,7 @@ function __sb_pundle_require_module(fromModule, request) {
   if (!(request in __sb_pundle.cache)) {
     throw new Error('Module not found')
   }
-  var module = __sb_pundle.cache[request]
+  const module = __sb_pundle.cache[request]
   if (module.parents.indexOf(fromModule) === -1 && fromModule !== '$root') {
     module.parents.push(fromModule)
   }
@@ -38,10 +49,10 @@ function __sb_pundle_require_module(fromModule, request) {
   return module.exports
 }
 function __sb_generate_require(moduleName) {
-  var bound = __sb_pundle_require_module.bind(null, moduleName)
+  const bound = __sb_pundle_require_module.bind(null, moduleName)
   bound.cache = __sb_pundle.cache
   bound.extensions = __sb_pundle.extensions
   bound.resolve = __sb_pundle.resolve
   return bound
 }
-var __require = __sb_generate_require('$root')
+__require = __sb_generate_require('$root');
