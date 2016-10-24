c0008495281b09e770b6de86b6a539d6d9ac7243
Steel Brain <i@steelbrain.me>
2016-10-24 09:49:16 +0530

    Revert ":fire: Nuke jasmine flow decls"
    This reverts commit 7b6dec9290eb637fa8d645b8b0ed95b4d66fb3a5.

---
+++ flow-decls/jasmine.js
@@ -0,0 +1,6 @@
+/* @flow */
+
+declare function it(name: string, callback: (() => void)): void;
+declare function fit(name: string, callback: (() => void)): void;
+declare function expect(value: any): Object;
+declare function describe(name: string, callback: (() => void)): void;
