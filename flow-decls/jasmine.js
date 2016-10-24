7b6dec9290eb637fa8d645b8b0ed95b4d66fb3a5
Steel Brain <i@steelbrain.me>
2016-10-24 09:11:22 +0530

    :fire: Nuke jasmine flow decls

--- flow-decls/jasmine.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-
-declare function it(name: string, callback: (() => void)): void;
-declare function fit(name: string, callback: (() => void)): void;
-declare function expect(value: any): Object;
-declare function describe(name: string, callback: (() => void)): void;
