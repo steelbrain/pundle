78f3480f46e505baa77475822485312e3bd3e0b9
Steel Brain <i@steelbrain.me>
2016-10-24 19:09:52 +0530

    ✅ Add Component specs in pundle-api

---
+++ packages/api/spec/component-spec.js
@@ -0,0 +1,258 @@
+/* @flow */
+
+import * as Components from '../src/components'
+
+describe('Components', function() {
+  describe('createLoader', function() {
+    function makeRequest(a: any, b: any) {
+      return Components.createLoader(a, b)
+    }
+
+    it('throws an error if parameter 1 is not a function', function() {
+      expect(function() {
+        makeRequest(null, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest({}, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest(false, {})
+      }).toThrow('Parameter 1 must be a function')
+    })
+    it('throws an error if parameter 2 is not an object', function() {
+      expect(function() {
+        makeRequest(function() {}, null)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, false)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, true)
+      }).toThrow('Parameter 2 must be an object')
+    })
+    it('returns expected results if both parameters are correct', function() {
+      const callback = function() { }
+      const defaultConfig = {}
+      const value = makeRequest(callback, defaultConfig)
+      expect(value.$type).toBe('loader')
+      expect(value.callback).toBe(callback)
+      expect(value.defaultConfig).toBe(defaultConfig)
+    })
+  })
+  describe('createPlugin', function() {
+    function makeRequest(a: any, b: any) {
+      return Components.createPlugin(a, b)
+    }
+
+    it('throws an error if parameter 1 is not a function', function() {
+      expect(function() {
+        makeRequest(null, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest({}, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest(false, {})
+      }).toThrow('Parameter 1 must be a function')
+    })
+    it('throws an error if parameter 2 is not an object', function() {
+      expect(function() {
+        makeRequest(function() {}, null)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, false)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, true)
+      }).toThrow('Parameter 2 must be an object')
+    })
+    it('returns expected results if both parameters are correct', function() {
+      const callback = function() { }
+      const defaultConfig = {}
+      const value = makeRequest(callback, defaultConfig)
+      expect(value.$type).toBe('plugin')
+      expect(value.callback).toBe(callback)
+      expect(value.defaultConfig).toBe(defaultConfig)
+    })
+  })
+  describe('createResolver', function() {
+    function makeRequest(a: any, b: any) {
+      return Components.createResolver(a, b)
+    }
+
+    it('throws an error if parameter 1 is not a function', function() {
+      expect(function() {
+        makeRequest(null, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest({}, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest(false, {})
+      }).toThrow('Parameter 1 must be a function')
+    })
+    it('throws an error if parameter 2 is not an object', function() {
+      expect(function() {
+        makeRequest(function() {}, null)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, false)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, true)
+      }).toThrow('Parameter 2 must be an object')
+    })
+    it('returns expected results if both parameters are correct', function() {
+      const callback = function() { }
+      const defaultConfig = {}
+      const value = makeRequest(callback, defaultConfig)
+      expect(value.$type).toBe('resolver')
+      expect(value.callback).toBe(callback)
+      expect(value.defaultConfig).toBe(defaultConfig)
+    })
+  })
+  describe('createReporter', function() {
+    function makeRequest(a: any, b: any) {
+      return Components.createReporter(a, b)
+    }
+
+    it('throws an error if parameter 1 is not a function', function() {
+      expect(function() {
+        makeRequest(null, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest({}, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest(false, {})
+      }).toThrow('Parameter 1 must be a function')
+    })
+    it('throws an error if parameter 2 is not an object', function() {
+      expect(function() {
+        makeRequest(function() {}, null)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, false)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, true)
+      }).toThrow('Parameter 2 must be an object')
+    })
+    it('returns expected results if both parameters are correct', function() {
+      const callback = function() { }
+      const defaultConfig = {}
+      const value = makeRequest(callback, defaultConfig)
+      expect(value.$type).toBe('reporter')
+      expect(value.callback).toBe(callback)
+      expect(value.defaultConfig).toBe(defaultConfig)
+    })
+  })
+  describe('createGenerator', function() {
+    function makeRequest(a: any, b: any) {
+      return Components.createGenerator(a, b)
+    }
+
+    it('throws an error if parameter 1 is not a function', function() {
+      expect(function() {
+        makeRequest(null, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest({}, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest(false, {})
+      }).toThrow('Parameter 1 must be a function')
+    })
+    it('throws an error if parameter 2 is not an object', function() {
+      expect(function() {
+        makeRequest(function() {}, null)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, false)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, true)
+      }).toThrow('Parameter 2 must be an object')
+    })
+    it('returns expected results if both parameters are correct', function() {
+      const callback = function() { }
+      const defaultConfig = {}
+      const value = makeRequest(callback, defaultConfig)
+      expect(value.$type).toBe('generator')
+      expect(value.callback).toBe(callback)
+      expect(value.defaultConfig).toBe(defaultConfig)
+    })
+  })
+  describe('createTransformer', function() {
+    function makeRequest(a: any, b: any) {
+      return Components.createTransformer(a, b)
+    }
+
+    it('throws an error if parameter 1 is not a function', function() {
+      expect(function() {
+        makeRequest(null, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest({}, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest(false, {})
+      }).toThrow('Parameter 1 must be a function')
+    })
+    it('throws an error if parameter 2 is not an object', function() {
+      expect(function() {
+        makeRequest(function() {}, null)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, false)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, true)
+      }).toThrow('Parameter 2 must be an object')
+    })
+    it('returns expected results if both parameters are correct', function() {
+      const callback = function() { }
+      const defaultConfig = {}
+      const value = makeRequest(callback, defaultConfig)
+      expect(value.$type).toBe('transformer')
+      expect(value.callback).toBe(callback)
+      expect(value.defaultConfig).toBe(defaultConfig)
+    })
+  })
+  describe('createPostTransformer', function() {
+    function makeRequest(a: any, b: any) {
+      return Components.createPostTransformer(a, b)
+    }
+
+    it('throws an error if parameter 1 is not a function', function() {
+      expect(function() {
+        makeRequest(null, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest({}, {})
+      }).toThrow('Parameter 1 must be a function')
+      expect(function() {
+        makeRequest(false, {})
+      }).toThrow('Parameter 1 must be a function')
+    })
+    it('throws an error if parameter 2 is not an object', function() {
+      expect(function() {
+        makeRequest(function() {}, null)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, false)
+      }).toThrow('Parameter 2 must be an object')
+      expect(function() {
+        makeRequest(function() {}, true)
+      }).toThrow('Parameter 2 must be an object')
+    })
+    it('returns expected results if both parameters are correct', function() {
+      const callback = function() { }
+      const defaultConfig = {}
+      const value = makeRequest(callback, defaultConfig)
+      expect(value.$type).toBe('post-transformer')
+      expect(value.callback).toBe(callback)
+      expect(value.defaultConfig).toBe(defaultConfig)
+    })
+  })
+})
