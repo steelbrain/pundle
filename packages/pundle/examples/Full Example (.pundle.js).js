3fe32d8311075ad07c1d4c91826e30f18a255768
Steel Brain <i@steelbrain.me>
2016-12-08 04:06:18 -0800

    :new: Add a new .pundle.js example

---
+++ packages/pundle/examples/Full Example (.pundle.js).js
@@ -0,0 +1,22 @@
+module.exports = {
+  debug: true,
+  // ^ Setting this to true sets "process.env.NODE_ENV" to "development" in processed js, it's set to "production" otherwise
+  entry: ['./index.js'],
+  output: {
+    bundlePath: '/bundle.js',
+    sourceMap: true,
+    sourceMapPath: '/bundle.js.map',
+  },
+  server: {
+    port: 8090,
+    hmrPath: '/__sb_pundle_shiny_hmr',
+    bundlePath: '/bundle.js',
+    sourceMapPath: '/bundle.js.map',
+    redirectNotFoundToIndex: true,
+  },
+  presets: [
+    [require.resolve('pundle-preset-default'), {
+      resolver: { extensions: ['', '.js', '.json', '.ts', '.tsx'] },
+    }],
+  ],
+}
