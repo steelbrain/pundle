c377f048b49558617e7fb3a87186833176579a0f
Steel Brain <i@steelbrain.me>
2016-12-24 23:48:19 -0800

    :bug: Fix sourcemap and root directory typos in cli

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -60,20 +60,21 @@ command
           sourceMap: config.server.sourceMap,
           sourceMapPath: config.server.sourceMapPath,
           bundlePath: config.server.bundlePath,
-          rootDirectory: options.rootDirectory || Path.resolve(options.serverRootDirectory, config.server.rootDirectory),
+          rootDirectory: options.serverRootDirectory || Path.resolve(options.rootDirectory, config.server.rootDirectory),
           redirectNotFoundToIndex: config.server.redirectNotFoundToIndex,
         }).then(function() {
           console.log(`Server is running on ${chalk.blue(`http://localhost:${serverPort}/`)}`)
         })
       } else {
         promise = pundle.generate(null, {
+          sourceMap: config.output.sourceMap,
           sourceMapPath: config.output.sourceMapPath,
         }).then(async function(generated) {
           const outputFilePath = Path.resolve(pundle.config.compilation.rootDirectory, config.output.bundlePath)
           const outputSourceMapPath = Path.resolve(pundle.config.compilation.rootDirectory, config.output.sourceMapPath)
           FS.writeFileSync(outputFilePath, generated.contents)
           console.log(`Wrote ${chalk.red(fileSize(generated.contents.length))} to '${chalk.blue(outputFilePath)}'`)
-          if (config.sourceMap && config.output.sourceMapPath !== 'inline') {
+          if (config.output.sourceMap && config.output.sourceMapPath !== 'inline') {
             const sourceMap = JSON.stringify(generated.sourceMap)
             FS.writeFileSync(outputSourceMapPath, sourceMap)
             console.log(`Wrote ${chalk.red(fileSize(sourceMap.length))} to '${chalk.blue(outputSourceMapPath)}'`)
