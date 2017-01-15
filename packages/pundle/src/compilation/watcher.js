7c6717cb33a4145bae0af6bb8372b5c7625a3b13
Steel Brain <i@steelbrain.me>
2017-01-15 18:55:39 +0500

    :arrow_down: Revert back to chokidar other watcher doesn't have the bug we were trying to escape, but it also is quite heavy on cpu during init and not as reliable in general

--- packages/pundle/src/compilation/watcher.js
+++ packages/pundle/src/compilation/watcher.js
@@ -1,64 +1,53 @@
 /* @flow */
 
-import watchr from 'watchr'
-import invariant from 'assert'
+import chokidar from 'chokidar'
 import { EventEmitter } from 'events'
 import type { WatcherConfig } from '../../types'
 
 export default class Watcher extends EventEmitter {
   paths: Map<string, number>;
+  active: boolean;
   config: WatcherConfig;
-  watchers: Map<string, Object>;
+  chokidar: Object;
 
   constructor(initialFiles: Array<string>, config: WatcherConfig) {
     super()
     this.paths = new Map()
+    this.active = true
     this.config = config
-    this.watchers = new Map()
+    this.chokidar = chokidar.watch([], {
+      usePolling: config.usePolling,
+    })
+    this.chokidar.on('unlink', filePath => this.emit('unlink', filePath))
+    this.chokidar.on('change', filePath => this.emit('change', filePath))
 
-    initialFiles.forEach(file => this.watch(file))
+    setImmediate(() => {
+      initialFiles.forEach(file => this.watch(file))
+    })
   }
   watch(filePath: string): void {
     const count = this.paths.get(filePath) || 0
-    this.paths.set(filePath, count)
+    const newCount = count + 1
+    this.paths.set(filePath, newCount)
 
-    if (!this.watchers.has(filePath)) {
-      const watcher = watchr.create(filePath)
-      watcher.setConfig({
-        interval: 100,
-        followLinks: true,
-        catchupDelay: 5,
-        preferredMethods: this.config.usePolling ? ['watchFile'] : ['watch', 'watchFile'],
-      })
-      watcher.on('change', (type) => this.emit(type === 'update' ? 'change' : 'unlink', filePath))
-      watcher.watch({}, function() {})
-      this.watchers.set(filePath, watcher)
+    if (newCount === 1) {
+      this.chokidar.add(filePath)
     }
   }
   unwatch(filePath: string): void {
     const count = this.paths.get(filePath) || 0
-    if (count === 0) {
-      return
+    const newCount = count - 1
+    if (newCount < 1) {
+      this.chokidar.unwatch(filePath)
+      this.paths.delete(filePath)
     }
-    if (count > 1) {
-      this.paths.set(filePath, count - 1)
-      return
-    }
-    const watcher = this.watchers.get(filePath)
-
-    invariant(watcher)
-    watcher.close()
-    this.paths.delete(filePath)
-    this.watchers.delete(filePath)
   }
   getWatchedFiles(): Array<string> {
     return Array.from(this.paths.keys())
   }
   dispose() {
-    for (const watcher of this.watchers.values()) {
-      watcher.close()
-    }
     this.paths.clear()
-    this.watchers.clear()
+    this.chokidar.close()
+    this.removeAllListeners()
   }
 }
