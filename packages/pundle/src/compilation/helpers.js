8d82aa2daddb3fbefa9c379fb646cf95e105e517
Steel Brain <i@steelbrain.me>
2017-01-15 12:41:55 +0500

    :fire: Remove an unrequired fix

--- packages/pundle/src/compilation/helpers.js
+++ packages/pundle/src/compilation/helpers.js
@@ -206,10 +206,14 @@ export async function processWatcherFileTree(
   }
 }
 
+// NOTE: The reason we only count in loaders and not transformers even though they could be useful
+// in cases like typescript is because the typescript preset includes it's own resolver.
+// and it includes it's resolver because if the user decides to specify ext for the default preset one
+// it'll break and users won't know why. so it just increases predictability.
 export function getAllKnownExtensions(components: Set<ComponentEntry>): Array<string> {
   let toReturn = ['']
   for (const entry of components) {
-    if (entry.component.$type === 'loader' || entry.component.$type === 'transformer') {
+    if (entry.component.$type === 'loader') {
       if (Array.isArray(entry.config.extensions)) {
         toReturn = toReturn.concat(entry.config.extensions)
       } else if (Array.isArray(entry.component.defaultConfig.extensions)) {
