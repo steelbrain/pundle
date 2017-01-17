bebcedfef68df451252afdff3d5d6f1e4d456fbc
Steel Brain <i@steelbrain.me>
2017-01-17 15:25:29 +0500

    :art: Accept HMR in example app

--- packages/cli/vendor/index.js
+++ packages/cli/vendor/index.js
@@ -4,3 +4,8 @@ const content = 'Hello World'
 const container = document.getElementById('app')
 
 container.textContent = content
+
+if (module.hot) {
+  // Accept Hot Module Replacement
+  module.hot.accept()
+}
