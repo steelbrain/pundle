bd6816aaeebed8f43fe8edd7d963b0034da019ce
Steel Brain <i@steelbrain.me>
2017-02-16 00:19:23 +0500

    :bug: Refactor dev server fixing race conditions

--- packages/dev/types.js
+++ packages/dev/types.js
@@ -1,19 +1,44 @@
 /* @flow */
 
-export type MiddlewareConfig = {
+import type { File } from 'pundle-api/types'
+
+export type ServerConfig = {
+  hmrHost: ?string,
+  hmrPath: string,
+  useCache: boolean,
+  hmrReports: boolean,
+  bundlePath: string,
+  sourceMap: boolean,
+  sourceMapPath: 'inline' | string,
+  redirectNotFoundToIndex: boolean,
+  port: number,
+  rootDirectory: string,
+}
+
+export type ServerConfigInput = {
   hmrHost: ?string,
   // NOTE: Must specify protocol, for example "https://google.com"
   hmrPath: ?string,
   // NOTE: ^ Set to null to disable hmr
+  useCache?: boolean,
   hmrReports?: boolean,
-  bundlePath: string,
-  sourceMap: boolean,
-  sourceMapPath: 'none' | 'inline' | string,
-}
+  // NOTE: ^ Set to false to hide `Sending HMR` reports
+  bundlePath?: string,
+  sourceMap?: boolean,
+  sourceMapPath?: 'inline' | string,
+  redirectNotFoundToIndex?: boolean,
+  // ^ Setting this to true makes it redirect all 404 requests to index
 
-export type ServerConfig = {
+  // These configs are required
   port: number,
   rootDirectory: string,
-  redirectNotFoundToIndex: boolean,
-  // ^ Setting this to true means it'll redirect all 404 requests to index
+}
+
+export type ServerState = {
+  files: Array<File>,
+  queue: Promise<void>,
+  booted: boolean,
+  modified: boolean,
+  activated: boolean,
+  generated: { contents: string, sourceMap: Object, filePaths: Array<string> },
 }
