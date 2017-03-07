a990773b5f063051e61937d7b572a49e86acff42
Steel Brain <i@steelbrain.me>
2017-03-07 19:39:12 +0500

    :new: Add support for common config in preests

--- packages/preset-typescript/src/index.js
+++ packages/preset-typescript/src/index.js
@@ -1,24 +1,25 @@
 /* @flow */
 
+const extensions = ['ts', 'tsx']
 export default [
   {
     name: 'loader',
     config: {
-      extensions: ['ts', 'tsx'],
+      extensions,
     },
     component: require.resolve('pundle-loader-js'),
   },
   {
     name: 'transformer',
     config: {
-      extensions: ['ts', 'tsx'],
+      extensions,
     },
     component: require.resolve('pundle-transformer-typescript'),
   },
   {
     name: 'resolver',
     config: {
-      extensions: ['ts', 'tsx'],
+      extensions,
       packageMains: ['browser', 'browserify', 'webpack', 'typescript:main', 'module', 'main'],
     },
     component: require.resolve('pundle-resolver-default'),
