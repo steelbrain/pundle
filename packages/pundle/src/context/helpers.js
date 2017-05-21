6d2f4772c142c527b02fffb5670d892f9e91c220
Steel Brain <i@steelbrain.me>
2017-05-22 04:50:48 +0500

    :art: Replace homegrown function with npm module

--- packages/pundle/src/context/helpers.js
+++ packages/pundle/src/context/helpers.js
@@ -2,7 +2,7 @@
 
 import unique from 'lodash.uniq'
 import invariant from 'assert'
-import sourceMap from 'source-map'
+import mergeSourceMap from 'merge-source-map'
 import type { Context, File } from 'pundle-api/types'
 import type { ComponentEntry } from '../../types'
 
@@ -47,46 +47,6 @@ export function getAllKnownExtensions(components: Set<ComponentEntry>): Array<st
     }
   }
   return unique(toReturn)
-}
-
-// Shamelessly copied from babel/babel under MIT License
-export function mergeSourceMap(inputMap: Object, map: Object): Object {
-  const inputMapConsumer = new sourceMap.SourceMapConsumer(inputMap)
-  const outputMapConsumer = new sourceMap.SourceMapConsumer(map)
-
-  const mergedGenerator = new sourceMap.SourceMapGenerator({
-    file: inputMapConsumer.file,
-    sourceRoot: inputMapConsumer.sourceRoot,
-    skipValidation: true,
-  })
-
-  // This assumes the output map always has a single source, since Babel always compiles a single source file to a
-  // single output file.
-  const source = outputMapConsumer.sources[0]
-
-  inputMapConsumer.eachMapping(function(mapping) {
-    const generatedPosition = outputMapConsumer.generatedPositionFor({
-      line: mapping.generatedLine,
-      column: mapping.generatedColumn,
-      source,
-    })
-    if (typeof generatedPosition.column !== 'undefined') {
-      mergedGenerator.addMapping({
-        source: mapping.source,
-
-        original: !mapping.source ? null : {
-          line: mapping.originalLine,
-          column: mapping.originalColumn,
-        },
-
-        generated: generatedPosition,
-      })
-    }
-  })
-
-  const mergedMap = mergedGenerator.toJSON()
-  inputMap.mappings = mergedMap.mappings
-  return inputMap
 }
 
 // Notes:
