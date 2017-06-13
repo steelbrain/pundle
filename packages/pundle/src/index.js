a1691519fcd91eb59130fc9164dfcf85d41b9bec
Steel Brain <i@steelbrain.me>
2017-06-14 03:11:14 +0500

    :new: Temporarily disable resuming from cache

--- packages/pundle/src/index.js
+++ packages/pundle/src/index.js
@@ -82,7 +82,9 @@ class Pundle {
     })
   }
   getCachedFiles(cache: ConfigFile, oldFiles: Map<string, File> = new Map()): Map<string, File> {
-    return Helpers.unserializeFiles(cache.getSync('files'), oldFiles)
+    // return Helpers.unserializeFiles(cache.getSync('files'), oldFiles)
+    // TODO: This is temporary because resuming from cache is broken with new File split to pundle-api
+    return new Map()
   }
   setCachedFiles(cache: ConfigFile, files: Map<string, File>): void {
     cache.setSync('files', Helpers.serializeFiles(files))
