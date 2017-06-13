8e9dae5950ee1c21fd0997eafd3017d4e4fd5527
Steel Brain <i@steelbrain.me>
2017-06-14 03:04:52 +0500

    :art: Minor change to default template

--- packages/motion/template/bundle/main.js
+++ packages/motion/template/bundle/main.js
@@ -2,10 +2,10 @@
 import React from 'react'
 
 export default class Main extends React.Component {
-  state = { name: 'John Green' }
+  state = { name: 'Bots, Probably' }
   render() {
     return (
-      <h1>Hello World ~ ${this.state.name}</h1>
+      <h1>Hello World ~ {this.state.name}</h1>
     )
   }
 }
