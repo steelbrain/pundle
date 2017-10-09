9b85bfa7bbf72bbf9a2c9d11778514642eb24e77
Steel Brain <i@steelbrain.me>
2017-10-09 04:52:32 -0700

    :art: Use callback style decl method in reporter-console

--- packages/pundle-reporter-console/src/index.js
+++ packages/pundle-reporter-console/src/index.js
@@ -5,13 +5,15 @@ import parseError from 'pundle-reporter-base'
 
 import { version } from '../package.json'
 
-export default registerComponent({
-  name: 'pundle-reporter-console',
-  version,
-  hookName: 'report',
-  callback(error) {
-    const parsed = parseError(error)
-    console.error(parsed)
-  },
-  defaultOptions: {},
-})
+export default function() {
+  return registerComponent({
+    name: 'pundle-reporter-console',
+    version,
+    hookName: 'report',
+    callback(error) {
+      const parsed = parseError(error)
+      console.error(parsed)
+    },
+    defaultOptions: {},
+  })
+}
