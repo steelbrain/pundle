6c990cbe19616df4c7e6549961b3981fbb6e149a
Steel Brain <i@steelbrain.me>
2018-05-11 13:11:20 +0500

    :new: Add a loader-json

--- packages/pundle-loader-json/lib/index.js
+++ packages/pundle-loader-json/lib/index.js
@@ -5,17 +5,26 @@ import { createFileLoader } from 'pundle-api'
 
 import manifest from '../package.json'
 
-export default function({ extensions = ['.js', '.mjs'] }: { extensions?: Array<string> } = {}) {
+export default function({ extensions = ['.json'] }: { extensions?: Array<string> } = {}) {
   return createFileLoader({
-    name: 'pundle-loader-js',
+    name: 'pundle-loader-json',
     version: manifest.version,
     callback({ contents, filePath, format }) {
       const extName = path.extname(filePath)
       if (!extensions.includes(extName) || format !== 'js') {
         return null
       }
+      let parsed
+      try {
+        parsed = contents.toString()
+      } catch (error) {
+        if (error instanceof SyntaxError) {
+          throw new Error(`Error parsing JSON at '${filePath}'`)
+        }
+        throw error
+      }
       return {
-        contents: contents.toString(),
+        contents: `module.exports = ${JSON.stringify(parsed)}`,
         isBuffer: false,
         sourceMap: null,
       }
