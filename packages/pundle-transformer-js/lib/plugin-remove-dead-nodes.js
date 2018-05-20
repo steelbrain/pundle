4f223de7cfab259e83847b8f331dea0fc0e367ec
Steel Brain <i@steelbrain.me>
2018-05-20 23:19:49 +0500

    :new: Support ternary in dead code removal plugin

--- packages/pundle-transformer-js/lib/plugin-remove-dead-nodes.js
+++ packages/pundle-transformer-js/lib/plugin-remove-dead-nodes.js
@@ -1,28 +1,39 @@
 // @flow
 
+import * as t from '@babel/types'
+
 // Empty out bodies of falsy parts of if/else statements
 // to avoid requiring modules that aren't needed aka
 // if (process.env.NODE_ENV === 'production') module.exports = require('./prod-version') else module.exports = require('./dev-version')
 // OR
 // module.exports = process.env.NODE_ENV === 'production' ? require('./prod-version') : require('./dev-version')
 
+function processBooleanConditional(path) {
+  const { node } = path
+  const { test, consequent, alternate } = node
+
+  if (test && test.type === 'BooleanLiteral') {
+    if (test.value) {
+      if (alternate) {
+        if (t.isBlockStatement(alternate)) {
+          node.alternate = null
+        } else {
+          node.alternate = t.numericLiteral(0)
+        }
+      }
+    } else {
+      consequent.body = []
+    }
+  }
+}
+
 export default {
   visitor: {
     IfStatement: {
-      exit(path) {
-        const { node } = path
-        const { test, consequent } = node
-
-        if (test && test.type === 'BooleanLiteral') {
-          if (test.value) {
-            if (node.alternate) {
-              node.alternate = null
-            }
-          } else {
-            consequent.body = []
-          }
-        }
-      },
+      exit: processBooleanConditional,
+    },
+    ConditionalExpression: {
+      exit: processBooleanConditional,
     },
   },
 }
