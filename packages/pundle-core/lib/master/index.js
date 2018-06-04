255cc9c9ad0c914c257a8df7ab2a0a87881271c2
Steel Brain <i@steelbrain.me>
2018-06-04 15:03:10 +0500

    :art: Minor code structure change

--- packages/pundle-core/lib/master/index.js
+++ packages/pundle-core/lib/master/index.js
@@ -221,38 +221,40 @@ export default class Master implements PundleWorker {
     )).map(resolved => getChunk(resolved.format, null, resolved.filePath))
 
     const tickCallback = async (oldFile: ?ImportTransformed, newFile: ImportTransformed) => {
+      let removedChunks = []
+      let removedImports = []
       if (oldFile) {
-        const removedChunks = differenceBy(oldFile.chunks, newFile.chunks, getChunkKey)
-        const removedImports = differenceBy(oldFile.imports, newFile.imports, getFileKey)
-        removedImports.forEach((fileImport: ImportResolved) => {
-          const fileImportKey = getFileKey(fileImport)
-          let found = configChunks.find(chunk => getFileKey(chunk) === fileImportKey)
-          if (found) {
-            // DO NOT DELETE ENTRIES
-            return
-          }
-          job.files.forEach((file: ImportTransformed) => {
-            found = found || file.imports.some(item => getFileKey(item) === fileImportKey) !== -1
-          })
-          if (!found) {
-            job.files.delete(fileImportKey)
-          }
-        })
-        removedChunks.forEach((chunk: Chunk) => {
-          const chunkKey = getChunkKey(chunk)
-          let found = configChunks.find(configChunk => getChunkKey(configChunk) === chunkKey)
-          if (found) {
-            // DO NOT DELETE ENTRIES
-            return
-          }
-          job.files.forEach((file: ImportTransformed) => {
-            found = found || file.chunks.some(item => getChunkKey(item) === chunkKey)
-          })
-          if (!found) {
-            job.chunks.delete(chunkKey)
-          }
-        })
+        removedChunks = differenceBy(oldFile.chunks, newFile.chunks, getChunkKey)
+        removedImports = differenceBy(oldFile.imports, newFile.imports, getFileKey)
       }
+      removedImports.forEach((fileImport: ImportResolved) => {
+        const fileImportKey = getFileKey(fileImport)
+        let found = configChunks.find(chunk => getFileKey(chunk) === fileImportKey)
+        if (found) {
+          // DO NOT DELETE ENTRIES
+          return
+        }
+        job.files.forEach((file: ImportTransformed) => {
+          found = found || file.imports.some(item => getFileKey(item) === fileImportKey) !== -1
+        })
+        if (!found) {
+          job.files.delete(fileImportKey)
+        }
+      })
+      removedChunks.forEach((chunk: Chunk) => {
+        const chunkKey = getChunkKey(chunk)
+        let found = configChunks.find(configChunk => getChunkKey(configChunk) === chunkKey)
+        if (found) {
+          // DO NOT DELETE ENTRIES
+          return
+        }
+        job.files.forEach((file: ImportTransformed) => {
+          found = found || file.chunks.some(item => getChunkKey(item) === chunkKey)
+        })
+        if (!found) {
+          job.chunks.delete(chunkKey)
+        }
+      })
       try {
         if (tick) {
           tick({ context, job, oldFile, newFile })
