dd0f2657acbf271dec811c474090ea3797b967bd
Steel Brain <i@steelbrain.me>
2018-06-13 07:34:53 +0500

    :art: A bit aggressive uglification

--- packages/pundle-chunk-transformer-js-uglify/lib/index.js
+++ packages/pundle-chunk-transformer-js-uglify/lib/index.js
@@ -6,14 +6,20 @@ import { createChunkTransformer } from 'pundle-api'
 import manifest from '../package.json'
 
 // TODO: Fix source maps
-function createComponent() {
+function createComponent({ options = {} }: { options?: Object } = {}) {
   return createChunkTransformer({
     name: 'pundle-chunk-transformer-js-uglify',
     version: manifest.version,
     async callback({ format, contents }) {
       if (format !== 'js') return null
 
-      const { code, error } = minify(typeof contents === 'string' ? contents : contents.toString())
+      const { code, error } = minify(typeof contents === 'string' ? contents : contents.toString(), {
+        compress: {
+          keep_fargs: false,
+          ...(options && options.compress ? options.compress : {}),
+        },
+        ...options,
+      })
       if (error) {
         throw new Error(error)
       }
