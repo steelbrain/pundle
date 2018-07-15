7e521777331e3c2a30b80e15fd7db58839b2329c
Steel Brain <i@steelbrain.me>
2018-07-15 16:22:48 +0500

    :bug: Fix some issues with react app template

--- packages/create-pundle-react-app/template/src/index.js
+++ packages/create-pundle-react-app/template/src/index.js
@@ -1,9 +1,10 @@
+import * as React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 
 const root = document.getElementById('root')
 
-render(App, root)
+render(<App />, root)
 
 // Enables HMR when in dev
 if (process.env.NODE_ENV === 'development' && module.hot) {
