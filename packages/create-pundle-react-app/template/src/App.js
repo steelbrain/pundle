581641163c1ae6661650f56ce7cf8f31dc8d470a
Steel Brain <i@steelbrain.me>
2018-07-21 23:41:09 +0500

    :bug: Fix usage of react-hot-loader

--- packages/create-pundle-react-app/template/src/App.js
+++ packages/create-pundle-react-app/template/src/App.js
@@ -1,6 +1,7 @@
+import { hot } from 'react-hot-loader'
 import * as React from 'react'
 import './App.css'
 
 const MyApp = () => <div>Hello World</div>
 
-export default MyApp
+export default hot(module)(MyApp)
--- packages/create-pundle-react-app/template/src/index.js
+++ packages/create-pundle-react-app/template/src/index.js
@@ -5,8 +5,3 @@ import App from './App'
 const root = document.getElementById('root')
 
 render(<App />, root)
-
-// Enables HMR when in dev
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept()
-}
